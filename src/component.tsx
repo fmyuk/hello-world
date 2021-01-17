081fb54a7d91a999d62e7d01698dfa1c928034ea
fumi uk <dcfumiya.238@gmail.com>
2021-01-18 08:19:48 +0900

    hello-world

--- src/component.tsx
+++ src/component.tsx
@@ -1,51 +1,90 @@
-import React, { useState, useCallback, useMemo } from "react"
+import React, { useState, useCallback, useMemo, useEffect, useRef, useReducer } from "react"
+import { reducer, initialState } from "./reducer"
 
-type Props = {
-  clickedX: number
-  clickedY: number
-  handleClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
-}
-
-const Component: React.FC<Props> = props => (
-  <div>
-    <div
-      style={{ width: 100, height: 100, background: "#ccf" }}
-      onClick={props.handleClick}
-    />
-    <p
-      style={{ width: 100, height: 100, background: "#fcc" }}
-      onClick={props.handleClick}
-    />
-    <p>X: {props.clickedX}</p>
-    <p>Y: {props.clickedY}</p>
-  </div>
-)
-
-const Container: React.FC = () => {
-  const [state, update] = useState({
-    clickedX: 0,
-    clickedY: 0
-  })
-  const handleClick = useCallback(
-    (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-      event.persist()
-      const { top, left } = event.currentTarget.getBoundingClientRect()
-      update(prev => ({
-        ...prev,
-        clickedX: event.clientX - left,
-        clickedY: event.clientY - top
-      }))
-    },
-    []
-  )
+const Component: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <Component
-      clickedX={state.clickedX}
-      clickedY={state.clickedY}
-      handleClick={handleClick}
-    />
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </>
   )
 }
+
+// const Component: React.FC = () => {
+//   const ref = useRef<null | HTMLDivElement>(null)
+//   useEffect(() => {
+//     if (ref.current === null) return
+//     const size = ref.current.getBoundingClientRect()
+//     console.log(size)
+//   })
+//   return (
+//     <div>
+//       <div ref={ref} style={{ width: 100, height: 100 }}></div>
+//     </div>
+//   )
+// }
+
+// const Component: React.FC = () => {
+//   const [count, setCount] = useState(0)
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCount(count + 1)
+//     }, 1000)
+//     return () => clearInterval(interval)
+//   })
+//   return (
+//     <div>{count}</div>
+//   )
+// }
+
+// type Props = {
+//   clickedX: number
+//   clickedY: number
+//   handleClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
+// }
+
+// const Component: React.FC<Props> = props => (
+//   <div>
+//     <div
+//       style={{ width: 100, height: 100, background: "#ccf" }}
+//       onClick={props.handleClick}
+//     />
+//     <p
+//       style={{ width: 100, height: 100, background: "#fcc" }}
+//       onClick={props.handleClick}
+//     />
+//     <p>X: {props.clickedX}</p>
+//     <p>Y: {props.clickedY}</p>
+//   </div>
+// )
+
+// const Container: React.FC = () => {
+//   const [state, update] = useState({
+//     clickedX: 0,
+//     clickedY: 0
+//   })
+//   const handleClick = useCallback(
+//     (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+//       event.persist()
+//       const { top, left } = event.currentTarget.getBoundingClientRect()
+//       update(prev => ({
+//         ...prev,
+//         clickedX: event.clientX - left,
+//         clickedY: event.clientY - top
+//       }))
+//     },
+//     []
+//   )
+//   return (
+//     <Component
+//       clickedX={state.clickedX}
+//       clickedY={state.clickedY}
+//       handleClick={handleClick}
+//     />
+//   )
+// }
 
 // const Component: React.FC = () => {
 //   const [count, setCount] = useState(0)
